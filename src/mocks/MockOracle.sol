5e2a904f8639057dac7b48761735f7af44e87ecc
thefinalb055 <finalb055@skiff.com>
2024-07-26 07:24:20 +0400

    refactor: :recycle: update mocks

--- src/mocks/MockOracle.sol
+++ src/mocks/MockOracle.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-
 import {IOracle} from "../interfaces/IOracle.sol";
 import {IMarket} from "../interfaces/IMarket.sol";
 
-contract Oracle is IOracle {
-    IMarket public market;
-
-    constructor(address _market) {
-        market = IMarket(_market);
-    }
-
+contract MockOracle is IOracle {
     function resolveMarket(
+        address market,
         bytes32 proposalId,
         IMarket.Outcome outcome
-    ) external override {
-        market.resolveMarket(proposalId, outcome);
+    ) external {
+        // Call the resolveMarket function on the FutarchyMarket contract
+        (bool success, ) = market.call(
+            abi.encodeWithSignature(
+                "resolveMarket(bytes32,uint8)",
+                proposalId,
+                outcome
+            )
+        );
+        require(success, "Failed to resolve market");
     }
 }
